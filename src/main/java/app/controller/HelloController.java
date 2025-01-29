c0896325b1484472be39b986f295db2a463110b8
Ubuntu <ubuntu@ip-172-31-93-158.ec2.internal>
2025-01-29 23:28:38 +0000

    Initial commit of HelloWorld project

---
+++ src/main/java/app/controller/HelloController.java
@@ -0,0 +1,13 @@
+package app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, World!";
+    }
+}
